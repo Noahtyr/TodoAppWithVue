3464308c14886eea0b6e0e6a8395ee7a549a891e
Noahtyr <Noahdavidian@hotmail.dk>
2018-05-15 09:01:21 +0200

    added small list, Indents are annoying!

---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <ul>
+            <li> Todo A </li>
+            <li> Todo B </li>
+            <li> Todo C </li>
+        </ul>
+    </div>
+</template>
+
+<script>export default {name: 'TodoList'}
+</script>
+<style scoped>
+</style>
